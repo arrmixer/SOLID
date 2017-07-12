e01fcf5d73720e91b7757c174b9be6a238b0b44b
Angel Rodriguez <arrmixer@gmail.com>
2017-07-12 11:36:49 -0400

    adding some more functionality to the app

--- src/com/AE/SingletonDesignPattern/App.java
+++ src/com/AE/SingletonDesignPattern/App.java
@@ -4,5 +4,15 @@ package com.AE.SingletonDesignPattern;
  * Created by Angel on 7/6/17.
  */
 public class App {
+    public static void main(String[] args) {
+        PerformanceStage stage = PerformanceStage.getInstance();
+
+        stage.turnOnLights();
+        System.out.println(stage.getCounter());
+
+        PerformanceStage stage2 = PerformanceStage.getInstance();
+        System.out.println(stage.getCounter());
+    }
+
 
 }
--- src/com/AE/SingletonDesignPattern/PerformanceStage.java
+++ src/com/AE/SingletonDesignPattern/PerformanceStage.java
@@ -4,7 +4,27 @@ package com.AE.SingletonDesignPattern;
  * Created by Angel on 7/6/17.
  */
 public class PerformanceStage {
+    private static int counter;
 
-    private PerformanceStage(){};
+    private static PerformanceStage INSTANCE = null;
+
+    private PerformanceStage(){
+        counter++;
+    }
+
+    public synchronized static PerformanceStage getInstance(){
+        if(INSTANCE == null){
+            INSTANCE = new PerformanceStage();
+        }
+        return INSTANCE;
+    }
+
+    public void turnOnLights(){
+        System.out.println("turn on lights...");
+    }
+
+    public int getCounter(){
+        return counter;
+    }
 
 }
